63ee02b386a0bb3e04394b83d81f6b5227829400
hbma <hbma@terdon.com>
2014-11-12 10:31:42 +0800

    update

--- protected/modules_core/space/messages/zh_cn/views_admin_modules.php
+++ protected/modules_core/space/messages/zh_cn/views_admin_modules.php
@@ -17,13 +17,13 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
-  '<strong>Space</strong> Modules' => '',
-  'Activated' => '',
-  'Are you sure? *ALL* module data for this space will be deleted!' => '',
-  'Configure' => '',
-  'Currently there are no modules available for this space!' => '',
-  'Disable' => '',
-  'Enable' => '',
-  'Enhance this space with modules.' => '',
-  'Modules' => '@@@@',
+  '<strong>Space</strong> Modules' => '<strong>模块</strong> 管理',
+  'Activated' => '已激活',
+  'Are you sure? *ALL* module data for this space will be deleted!' => '你确定吗？ 所有该版块的模块将被删除！',
+  'Configure' => '配置',
+  'Currently there are no modules available for this space!' => '当前版块没有可用模块！',
+  'Disable' => '失效',
+  'Enable' => '生效',
+  'Enhance this space with modules.' => '为版块添加模块',
+  'Modules' => '模块',
 );
